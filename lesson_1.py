2c7fef7627fcdc396d36f9cdc6debc4cdfe07c22
Vikrant Prasad <magicman@Sareenas-MacBook-Pro.local>
2021-09-03 21:19:21 -0700

    complete lesson 1

--- lesson_1.py
+++ lesson_1.py
@@ -7,4 +7,25 @@
 	# Is Active (named is_active)
 # Be sure to store each variable as the correct type!
 
-print("Hello World")
+team_number = 1956
+team_name = "Robo Ticks"
+location = "Davis, California"
+rookie_year = 2014
+is_active = False
+
+print("The team number is " + str(team_number))
+print("The team name is " + team_name)
+print("The team location is " + location)
+print("The teams rookie year was " + str(rookie_year))
+if is_active == True:
+	print("The team is active.")
+else:
+	print("The team is inactive.")
+
+if is_active == True:
+	print(team_name + ", an FRC team in " + location + ", has been an active team since " + str(rookie_year) + ".")
+else:
+	print(team_name + ", an FRC team in " + location + ", had its rookie year in " + str(rookie_year) + ", but currently the team is inactive.")
+
+mydict = {"hello": "bye"}
+print(mydict["hello"])
